5ab697979cec132de5d5e59f5c9e8a995cc0e4cf
Allan Egidio <allan.egidio@outlook.com>
2016-11-02 02:26:21 -0200

    Create CRUD operations

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 import express from 'express';
 import config from './config/config';
 import datasource from './config/datasource';
+import bodyParser from 'body-parser';
 
 //App
 const app = express();
 app.config = config;
 app.datasource = datasource(app);
 app.set('port', 8000);
+app.use(bodyParser.json());
 
 //models
 const Users = app.datasource.models.Users;
 
 app.route('/users')
+   .post((req, res) => {
+     Users.create(req.body)
+          .then(user => res.json(user))
+          .catch(error => res.status(412));
+   })
    .get((req, res) => {
       Users.findAll({})
-           .then(result => res.json(result))
+           .then(users => res.json(users))
            .catch(error => res.status(412));
    });
 
-
-
-
-
+app.route('/users/:id')
+   .get((req, res) => {
+      Users.findOne({ where: req.params })
+           .then(user => res.json(user))
+           .catch(error => res.status(412));
+   })
+   .put((req, res) => {
+      Users.update(req.body, { where: req.params })
+           .then(updatedUser => res.json(updatedUser))
+           .catch(error => res.status(412));
+   })
+   .delete((req, res) => {
+     Users.destroy({ where: req.params })
+          .then(deletedUser => res.sendStatus(204))
+          .catch(error => res.status(412));
+   });
 
 export default app;
--- test/integration/app.js
+++ test/integration/app.js
@@ -22,4 +22,59 @@ describe('User Books', () => {
             });
         });
     });
+
+    describe('Route GET /users/:id', () => {
+        it('Should return a user by id', (done) => {
+            request.get('/users/1')
+                   .end((error, res) => {
+                      console.log(res.body[0]);
+                      expect(res.body.id).to.be.eql(defaultUsers.id);
+                      expect(res.body.name).to.be.eql(defaultUsers.name);
+                      done(error);
+                   });
+        });
+    });
+
+    describe('Route POST /users', () => {
+        it('Should create a user', (done) => {
+            const newUser = {
+              id: 2,
+              name: 'Allan Egidio'
+            };
+
+            request.post('/users')
+                   .send(newUser)
+                   .end((error, res) => {
+                      expect(res.body.id).to.be.eql(newUser.id);
+                      expect(res.body.name).to.be.eql(newUser.name);
+                      done(error);
+                   });
+        });
+    });
+
+    describe('Route PUT /users/:id', () => {
+        it('Should update a user', (done) => {
+            const updateUser = {
+              id: 1,
+              name: 'Allan Egidio Atualizado'
+            };
+
+            request.put('/users/1')
+                   .send(updateUser)
+                   .end((error, res) => {
+                      expect(res.body).to.be.eql([1]);
+                      done(error);
+                   });
+        });
+    });
+
+    describe('Route DELETE /users/:id', () => {
+        it('Should delete a user', (done) => {
+            request.delete('/users/1')
+                   .end((error, res) => {
+                      expect(res.statusCode).to.be.eql(204);
+                      done(error);
+                   });
+        });
+    });
 });
